cac5dc8a095f977798a8d98f073daf9f643472e4
Natasha Ewing <natasharai@me.com>
2016-11-22 09:29:09 -0800

    changes made to keydown function

--- noise.js
+++ noise.js
@@ -1,54 +1,44 @@
 //// this works when I hardcode the sound ///
 
-  // $(document).ready(function(){
-  //   $(".instrument").on('click', 'button', function(event){
-      // alert('you pressed   ' + '  ' + $(this).html() + '');
-  //     var tone = new Audio('media/c_note.mp3');
-  //     tone.play();
-  //   });
-  // });
+// $(document).ready(function(){
+//   $(".instrument").on('click', 'button', function(event){
+// alert('you pressed   ' + '  ' + $(this).html() + '');
+//     var tone = new Audio('media/c_note.mp3');
+//     tone.play();
+//   });
+// });
 
 //// this is for the keydown function ///
 
+var noteFromKey = function(key){
+  var regex = /^[a-g]$/;
+  var note = (key.match(regex))? key : (" ");
+  return note;
+};
 
+var key = function(event){
+  var x = event.which;
+  var note = String.fromCharCode(x);
+  return note;
+};
 
-  var noteFromKey = function(key){
-    var note = (key.match(/^[a-g]$/))? key : " ";
-    return note;
-  };
+var audio = function(note){
+  return $("#" + note + "Audio");
+};
 
-  var key = function(event){
-    alert('you pressed   ' + '  ' + $(this).html() + '');
-    return $(this).html();
-  };
+var note = function(button){
+  return $(button).html();
+};
 
-  var audio = function(note){
-    return $("#" + note + "Audio");
-  };
-
-  var note = function(button){
-    return $(button).html();
-  };
-
-  var sounds = function(event){
-    console.log(event);
-    var tune = sounds().audio();
-    var tone = new Audio(tune);
-    tone.play();
-  };
-
-  $(document).ready(function(){
-    $(".instrument").on('click', 'button', function(event){
-      var tone = note(event.target);
-      var sound = audio(tone);
-      sound[0].play();
-    });
-  });
-
-  $(document).keydown(function(event){
-
-    var pressed = key(event);
-    var tone = noteFromKey(pressed);
+$(document).ready(function(){
+  $(".instrument").on('click', 'button', function(event){
+    var tone = note(event.target);
     var sound = audio(tone);
     sound[0].play();
   });
+    $(this).keydown(function(){
+    var tone = key(event);
+    var sound = audio(tone);
+    sound[0].play();
+  });
+});
