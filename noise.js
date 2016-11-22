8c7c776f332a99e3d65c5b806dc89750c87c488d
Natasha Ewing <natasharai@me.com>
2016-11-21 23:12:17 -0800

    progress on keyboard

--- noise.js
+++ noise.js
@@ -1,3 +1,54 @@
-$(document).ready( function() {
-  // your code here
-});
+//// this works when I hardcode the sound ///
+
+  // $(document).ready(function(){
+  //   $(".instrument").on('click', 'button', function(event){
+      // alert('you pressed   ' + '  ' + $(this).html() + '');
+  //     var tone = new Audio('media/c_note.mp3');
+  //     tone.play();
+  //   });
+  // });
+
+//// this is for the keydown function ///
+
+
+
+  var noteFromKey = function(key){
+    var note = (key.match(/^[a-g]$/))? key : " ";
+    return note;
+  };
+
+  var key = function(event){
+    alert('you pressed   ' + '  ' + $(this).html() + '');
+    return $(this).html();
+  };
+
+  var audio = function(note){
+    return $("#" + note + "Audio");
+  };
+
+  var note = function(button){
+    return $(button).html();
+  };
+
+  var sounds = function(event){
+    console.log(event);
+    var tune = sounds().audio();
+    var tone = new Audio(tune);
+    tone.play();
+  };
+
+  $(document).ready(function(){
+    $(".instrument").on('click', 'button', function(event){
+      var tone = note(event.target);
+      var sound = audio(tone);
+      sound[0].play();
+    });
+  });
+
+  $(document).keydown(function(event){
+
+    var pressed = key(event);
+    var tone = noteFromKey(pressed);
+    var sound = audio(tone);
+    sound[0].play();
+  });
